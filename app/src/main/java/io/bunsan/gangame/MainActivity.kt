4ede1165905da9a8912bb8945ee650d876bd84f1
Luisangonzalez <luisangonzalez@gmail.com>
2018-05-15 13:03:23 +0200

    add kotlin-android-extension and refactor activity main

--- app/src/main/java/io/bunsan/gangame/MainActivity.kt
+++ app/src/main/java/io/bunsan/gangame/MainActivity.kt
@@ -2,11 +2,16 @@ package io.bunsan.gangame
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    val DEFAULT_OPTION = R.id.action_deals
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        navigationView.selectedItemId = DEFAULT_OPTION
     }
 }
--- app/src/main/java/io/bunsan/gangame/deals/DealsFragment.kt
+++ app/src/main/java/io/bunsan/gangame/deals/DealsFragment.kt
@@ -1,15 +1,10 @@
 package io.bunsan.gangame.deals
 
-import android.app.Fragment
-import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import com.example.commons.BaseFragment
 import io.bunsan.gangame.R
 
-class DealsFragment() : Fragment() {
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
-        val view = inflater.inflate(R.layout.fragment_deals, container, false)
-        return view
+class DealsFragment() : BaseFragment() {
+    override fun getLayoutResId(): Int {
+        return  R.layout.fragment_deals
     }
 }
